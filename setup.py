4f2258e86743e8d60e35b4eb526461710d011aea
Nikolay Ivanov <peoplecantfly@gmail.com>
2016-04-20 17:52:17 +0600

    added setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#-*-: coding: utf-8
+
+from distutils.core import setup
+
+setup(
+	name='icapserver',
+	version='1.1',
+	description='ICAP Server framework',
+	author='Nikolay Ivanov',
+	author_email='peoplecantfly@gmail.com',
+	url='https://github.com/Peoplecantfly/icapserver',
+	download_url='https://github.com/Peoplecantfly/icapserver/releases/tag/v1.1',
+	license='MIT License',
+	platforms='OS Independent',
+	py_modules=['icapserver'],
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Plugins',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries'
+	]
+)
